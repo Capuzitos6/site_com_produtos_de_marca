6132a1091e0fd7c3d5841f9b23836c637887197a
Capuzitos6 <135024021+Capuzitos6@users.noreply.github.com>
2023-11-20 09:27:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function menuShow() {
     }
 }
 
-function changeImage() {
+function changeImageBranca() {
     var image = document.getElementById('camisa.branca');
     if (image.src.match("img/Dimona_Frente_branca.png")) {
         image.src = "img/Dimona_Costas_branca.png";
@@ -18,7 +18,7 @@ function changeImage() {
     }
 }
 
-function changeImage() {
+function changeImagePreta() {
     var image = document.getElementById('camisa.preta');
     if (image.src.match("img/Dimona_Frente_preta.png")) {
         image.src = "img/Dimona_Costas_preta.png";
@@ -26,3 +26,66 @@ function changeImage() {
         image.src = "img/Dimona_Frente_preta.png";
     }
 }
+
+function changeImageTime() {
+    var image = document.getElementById('camisa.time');
+    if (image.src.match("img/Dimona_Frente_time.png")) {
+        image.src = "img/Dimona_Costas_time.png";
+    } else {
+        image.src = "img/Dimona_Frente_time.png";
+    }
+}
+
+function changeImageKitty() {
+    var image = document.getElementById('camisa.Kitty');
+    if (image.src.match("img/Dimona_Frente_kitty.png")) {
+        image.src = "img/Dimona_Costas_kitty.png";
+    } else {
+        image.src = "img/Dimona_Frente_kitty.png";
+    }
+}
+
+function changeImagepromo() {
+    var image = document.getElementById('camisa.promo');
+    if (image.src.match("img/Promoção.2.png")) {
+        image.src = "img/Promoção.1.png";
+    } else {
+        image.src = "img/Promoção.2.png";
+    }
+}
+
+
+
+let currentIndex = 0;
+let images = document.querySelectorAll('.carousel img/Promoção.1.png');
+let points = document.querySelectorAll('.point');
+
+
+function updatePoints() {
+    points.forEach((point, index) => {
+        if (index === currentIndex) {
+            point.style.backgroundColor = '#000';
+        } else {
+            point.style.backgroundColor = '#ccc';
+        }
+    });
+}
+
+
+function moveCarousel(newIndex) {
+    if (newIndex >= 0 && newIndex < images.length) {
+        images[currentIndex].style.display = 'none';
+        images[newIndex].style.display = 'block';
+        currentIndex = newIndex;
+        updatePoints();
+    }
+}
+
+
+function startCarousel() {
+    setInterval(() => {
+        moveCarousel((currentIndex + 1) % images.length);
+    }, 3000);
+}
+
+startCarousel();
