966252655baf4c6e71b6caf490a14e1b03210070
Capuzitos6 <135024021+Capuzitos6@users.noreply.github.com>
2023-11-21 11:11:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,75 +2,101 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+        document.querySelector('.icon').src = "/img/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+        document.querySelector('.icon').src = "/img/close_white_36dp.svg";
     }
 }
 
-function changeImagemine() {
-    var image = document.getElementById('camisa.mine');
-    if (image.src.match("img/Dimona_Frente_Mine.png")) {
-        image.src = "img/Dimona_Costas_Mine.png";
-    } else {
-        image.src = "img/Dimona_Frente_Mine.png";
+ 
+
+{
+
+    function changeImagemine() {
+        var image = document.getElementById('camisa.mine');
+        if (image.src.match("img/Dimona_Frente_Mine.png")) {
+            image.src = "img/Dimona_Costas_Mine.png";
+        } else {
+            image.src = "img/Dimona_Frente_Mine.png";
+        }
     }
+    
+    function changeImagePreta() {
+        var image = document.getElementById('camisa.preta');
+        if (image.src.match("img/Dimona_Frente_preta.png")) {
+            image.src = "img/Dimona_Costas_preta.png";
+        } else {
+            image.src = "img/Dimona_Frente_preta.png";
+        }
+    }
+    
+    function changeImageTime() {
+        var image = document.getElementById('camisa.time');
+        if (image.src.match("img/Dimona_Frente_time.png")) {
+            image.src = "img/Dimona_Costas_time.png";
+        } else {
+            image.src = "img/Dimona_Frente_time.png";
+        }
+    }
+    
+    function changeImagegomu() {
+        var image = document.getElementById('camisa.gomu');
+        if (image.src.match("img/Dimona_Frente_Gomu.png")) {
+            image.src = "img/Dimona_Costas_Gomu.png";
+        } else {
+            image.src = "img/Dimona_Frente_Gomu.png";
+        }
+    }
+    
+    function changeImagedonuts() {
+        var image = document.getElementById('camisa.donuts');
+        if (image.src.match("img/Dimona_Frente_Donuts.png")) {
+            image.src = "img/Dimona_Costas_Donuts.png";
+        } else {
+            image.src = "img/Dimona_Frente_Donuts.png";
+        }
+    }
+    
+    function changeImageegg3() {
+        var image = document.getElementById('camisa.egg3');
+        if (image.src.match("img/Dimona_Frente_egg3.png")) {
+            image.src = "img/Dimona_Costas_egg3.png";
+        } else {
+            image.src = "img/Dimona_Frente_egg3.png";
+        }
+    }
+    
+    function changeImagetrauma() {
+        var image = document.getElementById('camisa.trauma');
+        if (image.src.match("img/Dimona_Frente_Trauma.png")) {
+            image.src = "img/Dimona_Costas_Trauma.png";
+        } else {
+            image.src = "img/Dimona_Frente_Trauma.png";
+        }
+    }
+
+    function changeImagetotoro() {
+        var image = document.getElementById('camisa.totoro');
+        if (image.src.match("img/Dimona_Frente_totoro.png")) {
+            image.src = "img/Dimona_Costas_totoro.png";
+        } else {
+            image.src = "img/Dimona_Frente_totoro.png";
+        }
+    }
+
+
 }
 
-function changeImagePreta() {
-    var image = document.getElementById('camisa.preta');
-    if (image.src.match("img/Dimona_Frente_preta.png")) {
-        image.src = "img/Dimona_Costas_preta.png";
-    } else {
-        image.src = "img/Dimona_Frente_preta.png";
-    }
-}
+ document.addEventListener('DOMContentLoaded', function () {
+    const botaoAdicionarAoCarrinho = document.querySelector('.adicionar-ao-carrinho');
+    const botaoComprarAgora = document.querySelector('.comprar-agora');
 
-function changeImageTime() {
-    var image = document.getElementById('camisa.time');
-    if (image.src.match("img/Dimona_Frente_time.png")) {
-        image.src = "img/Dimona_Costas_time.png";
-    } else {
-        image.src = "img/Dimona_Frente_time.png";
-    }
-}
+    botaoAdicionarAoCarrinho.addEventListener('click', function () {
+        alert('Tênis adicionado ao carrinho');
+    });
 
-function changeImagegomu() {
-    var image = document.getElementById('camisa.gomu');
-    if (image.src.match("img/Dimona_Frente_Gomu.png")) {
-        image.src = "img/Dimona_Costas_Gomu.png";
-    } else {
-        image.src = "img/Dimona_Frente_Gomu.png";
-    }
-}
-
-function changeImagedonuts() {
-    var image = document.getElementById('camisa.donuts');
-    if (image.src.match("img/Dimona_Frente_Donuts.png")) {
-        image.src = "img/Dimona_Costas_Donuts.png";
-    } else {
-        image.src = "img/Dimona_Frente_Donuts.png";
-    }
-}
-
-function changeImageegg3() {
-    var image = document.getElementById('camisa.egg3');
-    if (image.src.match("img/Dimona_Frente_egg3.png")) {
-        image.src = "img/Dimona_Costas_egg3.png";
-    } else {
-        image.src = "img/Dimona_Frente_egg3.png";
-    }
-}
-
-function changeImagetrauma() {
-    var image = document.getElementById('camisa.trauma');
-    if (image.src.match("img/Dimona_Frente_Trauma.png")) {
-        image.src = "img/Dimona_Costas_Trauma.png";
-    } else {
-        image.src = "img/Dimona_Frente_Trauma.png";
-    }
-}
-
-
-
+    botaoComprarAgora.addEventListener('click', function () {
+        alert('Compra realizada');
+    });
+});
